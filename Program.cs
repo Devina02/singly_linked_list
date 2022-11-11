0e81c4dab05d5b4c3e0b8724416dbe62e00c1771
Devina02 <devina.fitriapngs@gmail.com>
2022-11-11 20:29:09 +0700

    membuat case 2

--- Program.cs
+++ Program.cs
@@ -142,6 +142,22 @@ namespace singly_linked_list
                                 obj.addNote();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is Empty");
+                                    break;
+                                }
+                                Console.Write("\nEnter the roll number of" + " The student whose record is to be deleted : ");
+                                int nim = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNode(nim) == false)
+                                    Console.WriteLine("\n Record not found.");
+                                else
+                                    Console.WriteLine("Record with roll number " + nim + " Deleted");
+                            }
+                            break;
                     }
                     
                 }   
