83f16fbac17c1d607744ccf49ccbd06f411bc430
Devina02 <devina.fitriapngs@gmail.com>
2022-11-11 17:13:28 +0700

    membuat class Node

--- Program.cs
+++ Program.cs
@@ -7,4 +7,11 @@ using System.Threading.Tasks;
 namespace singly_linked_list
 {
   //each node consist of the information part and  lik to the next mode
+  
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
 }
